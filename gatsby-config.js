90b23fcde8a18eaeadd51360790bc2607d20caab
xemil <emil@crossplatform.se>
2019-01-12 16:17:31 +0100

    linting fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,85 +1,85 @@
 module.exports = {
-  siteMetadata: {
-    title: `My website`,
-    googleVerification: `abcdefz`,
-    disqus: `gatsby-typescript`
-  },
-  mapping: {
-    'MarkdownRemark.frontmatter.author': `AuthorJson`
-  },
-  plugins: [
-    // Expose `/data` to graphQL layer
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/data`
-      }
+    siteMetadata: {
+        title: `My website`,
+        googleVerification: `abcdefz`,
+        disqus: `gatsby-typescript`,
     },
-
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true
-      }
+    mapping: {
+        'MarkdownRemark.frontmatter.author': `AuthorJson`,
     },
-
-    // Parse all markdown files (each plugin add/parse some data into graphQL layer)
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    plugins: [
+        // Expose `/data` to graphQL layer
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 690,
-              backgroundColor: `#f7f0eb`
-            }
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-autolink-headers`
-        ]
-      }
-    },
+                name: `data`,
+                path: `${__dirname}/data`,
+            },
+        },
 
-    // Parse all images files
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+                // Puts tracking script in the head instead of the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+            },
+        },
 
-    // Parse JSON files
-    `gatsby-transformer-json`,
+        // Parse all markdown files (each plugin add/parse some data into graphQL layer)
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 690,
+                            backgroundColor: `#f7f0eb`,
+                        },
+                    },
+                    `gatsby-remark-prismjs`,
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-autolink-headers`,
+                ],
+            },
+        },
 
-    // Add typescript stack into webpack
-    `gatsby-plugin-typescript`,
+        // Parse all images files
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
 
-    // This plugin takes your configuration and generates a
-    // web manifest file so your website can be added to your
-    // homescreen on Android.
-    /* eslint-disable camelcase */
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby website`,
-        short_name: `Gatsby website`,
-        start_url: `/`,
-        background_color: `#f7f7f7`,
-        theme_color: `#191919`,
-        display: `minimal-ui`
-      }
-    },
-    /* eslint-enable camelcase */
+        // Parse JSON files
+        `gatsby-transformer-json`,
 
-    // This plugin generates a service worker and AppShell
-    // html file so the site works offline and is otherwise
-    // resistant to bad networks. Works with almost any
-    // site!
-    `gatsby-plugin-offline`
-  ]
+        // Add typescript stack into webpack
+        `gatsby-plugin-typescript`,
+
+        // This plugin takes your configuration and generates a
+        // web manifest file so your website can be added to your
+        // homescreen on Android.
+        /* eslint-disable camelcase */
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby website`,
+                short_name: `Gatsby website`,
+                start_url: `/`,
+                background_color: `#f7f7f7`,
+                theme_color: `#191919`,
+                display: `minimal-ui`,
+            },
+        },
+        /* eslint-enable camelcase */
+
+        // This plugin generates a service worker and AppShell
+        // html file so the site works offline and is otherwise
+        // resistant to bad networks. Works with almost any
+        // site!
+        `gatsby-plugin-offline`,
+    ],
 };
